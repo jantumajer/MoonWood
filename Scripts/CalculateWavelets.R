8dc6e04ac97188091cd4848b014f8613fe59c0f1
jantumajer <tumajer1@email.cz>
2023-05-25 11:37:41 +0200

    Update CalculateWavelets.R

--- Scripts/CalculateWavelets.R
+++ Scripts/CalculateWavelets.R
@@ -4,12 +4,12 @@ library(WaveletComp)
 ### Loading GRO and TWD data ###
 ################################
 
-ALL <- rbind(read.csv("E:/MoonWood/Manuscript/DataAvailability/Data/Acer.csv"),
-             read.csv("E:/MoonWood/Manuscript/DataAvailability/Data/Fagus.csv"),
-             read.csv("E:/MoonWood/Manuscript/DataAvailability/Data/Carpinus.csv"),
-             read.csv("E:/MoonWood/Manuscript/DataAvailability/Data/Quercus.csv"),
-             read.csv("E:/MoonWood/Manuscript/DataAvailability/Data/Pinus.csv"),
-             read.csv("E:/MoonWood/Manuscript/DataAvailability/Data/Picea.csv"))
+ALL <- rbind(read.csv("Acer.csv"),
+             read.csv("Fagus.csv"),
+             read.csv("Carpinus.csv"),
+             read.csv("Quercus.csv"),
+             read.csv("Pinus.csv"),
+             read.csv("Picea.csv"))
 
 ###################################
 ### Equalising freezing periods ###
@@ -28,38 +28,38 @@ wv.gro.A <- analyze.wavelet(my.data = ALL[ALL$Species == "A",],
                                my.series = "GRO",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.gro.A, "E:/MoonWood/Data/Waves/waves_calc/A_GRO_60min.Rda")
+saveRDS(wv.gro.A, "A_GRO_60min.Rda")
 
 wv.gro.B <- analyze.wavelet(my.data = ALL[ALL$Species == "B",],
                                my.series = "GRO",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.gro.B, "E:/MoonWood/Data/Waves/waves_calc/B_GRO_60min.Rda")
+saveRDS(wv.gro.B, "B_GRO_60min.Rda")
 
 wv.gro.C <- analyze.wavelet(my.data = ALL[ALL$Species == "C",],
                                my.series = "GRO",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.gro.C, "E:/MoonWood/Data/Waves/waves_calc/C_GRO_60min.Rda")
+saveRDS(wv.gro.C, "C_GRO_60min.Rda")
 
 wv.gro.O <- analyze.wavelet(my.data = ALL[ALL$Species == "O",],
                                my.series = "GRO",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.gro.O, "E:/MoonWood/Data/Waves/waves_calc/O_GRO_60min.Rda")
+saveRDS(wv.gro.O, "O_GRO_60min.Rda")
 
 wv.gro.PISY <- analyze.wavelet(my.data = ALL[ALL$Species == "PISY",],
                                my.series = "GRO",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.gro.PISY, "E:/MoonWood/Data/Waves/waves_calc/PISY_GRO_60min.Rda")
+saveRDS(wv.gro.PISY, "PISY_GRO_60min.Rda")
 
 
 wv.gro.PCAB <- analyze.wavelet(my.data = ALL[ALL$Species == "PCAB",],
                                my.series = "GRO",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.gro.PCAB, "E:/MoonWood/Data/Waves/waves_calc/PCAB_GRO_60min.Rda")
+saveRDS(wv.gro.PCAB, "PCAB_GRO_60min.Rda")
 
 ### TWD ###
 
@@ -67,37 +67,37 @@ wv.twd.A <- analyze.wavelet(my.data = ALL[ALL$Species == "A",],
                                my.series = "TWD",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.twd.A, "E:/MoonWood/Data/Waves/waves_calc/A_TWD_60min.Rda")
+saveRDS(wv.twd.A, "A_TWD_60min.Rda")
 
 wv.twd.B <- analyze.wavelet(my.data = ALL[ALL$Species == "B",],
                                my.series = "TWD",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.twd.B, "E:/MoonWood/Data/Waves/waves_calc/B_TWD_60min.Rda")
+saveRDS(wv.twd.B, "B_TWD_60min.Rda")
 
 wv.twd.C <- analyze.wavelet(my.data = ALL[ALL$Species == "C",],
                                my.series = "TWD",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.twd.C, "E:/MoonWood/Data/Waves/waves_calc/C_TWD_60min.Rda")
+saveRDS(wv.twd.C, "C_TWD_60min.Rda")
 
 wv.twd.O <- analyze.wavelet(my.data = ALL[ALL$Species == "O",],
                                my.series = "TWD",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.twd.O, "E:/MoonWood/Data/Waves/waves_calc/O_TWD_60min.Rda")
+saveRDS(wv.twd.O, "O_TWD_60min.Rda")
 
 wv.twd.PISY <- analyze.wavelet(my.data = ALL[ALL$Species == "PISY",],
                                my.series = "TWD",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.twd.PISY, "E:/MoonWood/Data/Waves/waves_calc/PISY_TWD_60min.Rda")
+saveRDS(wv.twd.PISY, "PISY_TWD_60min.Rda")
 
 wv.twd.PCAB <- analyze.wavelet(my.data = ALL[ALL$Species == "PCAB",],
                                my.series = "TWD",
                                lowerPeriod = 2, upperPeriod = 24*365*2,
                                loess.span = 0)
-saveRDS(wv.twd.PCAB, "E:/MoonWood/Data/Waves/waves_calc/PCAB_TWD_60min.Rda")
+saveRDS(wv.twd.PCAB, "PCAB_TWD_60min.Rda")
 
 ### Temperature ###
 
@@ -105,28 +105,28 @@ wv.temp.A <- analyze.wavelet(my.data = ALL[ALL$Species == "A",],
                              my.series = "Temperature",
                              lowerPeriod = 2, upperPeriod = 24*365*2,
                              loess.span = 0)
-saveRDS(wv.temp.A, "E:/MoonWood/Data/Waves/clim/A_t.Rda")
+saveRDS(wv.temp.A, "A_t.Rda")
 
 
 wv.temp.B <- analyze.wavelet(my.data = ALL[ALL$Species == "B",],
                              my.series = "Temperature",
                              lowerPeriod = 2, upperPeriod = 24*365*2,
                              loess.span = 0)
-saveRDS(wv.temp.B, "E:/MoonWood/Data/Waves/clim/B_t.Rda")
+saveRDS(wv.temp.B, "B_t.Rda")
 
 
 wv.temp.O <- analyze.wavelet(my.data = ALL[ALL$Species == "O",],
                              my.series = "Temperature",
                              lowerPeriod = 2, upperPeriod = 24*365*2,
                              loess.span = 0)
-saveRDS(wv.temp.O, "E:/MoonWood/Data/Waves/clim/O_t.Rda")
+saveRDS(wv.temp.O, "O_t.Rda")
 
 temp.c <- ALL[ALL$Species == "C",]
 wv.temp.C <- analyze.wavelet(my.data = temp.c[c(1:38750),],
                              my.series = "Temperature",
                              lowerPeriod = 2, upperPeriod = 24*365*2,
                              loess.span = 0)
-saveRDS(wv.temp.C, "E:/MoonWood/Data/Waves/clim/C_t.Rda")
+saveRDS(wv.temp.C, "C_t.Rda")
 
 temp.PISY <- ALL[ALL$Species == "PISY",]
 PISY.t <- analyze.wavelet(my.data = temp.PISY[c(1:32891),],
@@ -134,7 +134,7 @@ PISY.t <- analyze.wavelet(my.data = temp.PISY[c(1:32891),],
                           lowerPeriod = 2, upperPeriod = 24*365*2,
                           dj = 1/20,
                           loess.span = 0)
-saveRDS(PISY.t, "E:/MoonWood/Data/Zweifel/VPD_60min/waves/PISY_t.Rda")
+saveRDS(PISY.t, "PISY_t.Rda")
 
 temp.PCAB <- ALL[ALL$Species == "PCAB",]
 PCAB.t <- analyze.wavelet(my.data = temp.PCAB[c(1:38450),],
@@ -142,7 +142,7 @@ PCAB.t <- analyze.wavelet(my.data = temp.PCAB[c(1:38450),],
                           lowerPeriod = 2, upperPeriod = 24*365*2,
                           dj = 1/20,
                           loess.span = 0)
-saveRDS(PCAB.t, "E:/MoonWood/Data/Zweifel/VPD_60min/waves/PCAB_t.Rda")
+saveRDS(PCAB.t, "PCAB_t.Rda")
 
 ### VPD ###
 
@@ -151,21 +151,21 @@ wv.vpd.A <- analyze.wavelet(my.data = a.vpd[c(14775:52608),],
                             my.series = "VPD",
                             lowerPeriod = 2, upperPeriod = 24*365*2,
                             loess.span = 0)
-saveRDS(wv.vpd.A, "E:/MoonWood/Data/Waves/clim/A_vpd.Rda")
+saveRDS(wv.vpd.A, "A_vpd.Rda")
 
 b.vpd <- ALL[ALL$Species == "B",]
 wv.vpd.B <- analyze.wavelet(my.data = b.vpd[c(14775:52608),],
                             my.series = "VPD",
                             lowerPeriod = 2, upperPeriod = 24*365*2,
                             loess.span = 0)
-saveRDS(wv.vpd.B, "E:/MoonWood/Data/Waves/clim/B_vpd.Rda")
+saveRDS(wv.vpd.B, "B_vpd.Rda")
 
 o.vpd <- ALL[ALL$Species == "O",]
 wv.vpd.O <- analyze.wavelet(my.data = o.vpd[c(14775:52608),],
                             my.series = "VPD",
                             lowerPeriod = 2, upperPeriod = 24*365*2,
                             loess.span = 0)
-saveRDS(wv.vpd.O, "E:/MoonWood/Data/Waves/clim/O_vpd.Rda")
+saveRDS(wv.vpd.O, "O_vpd.Rda")
 
 vpd.PISY <- ALL[ALL$Species == "PISY",]
 PISY.vpd <- analyze.wavelet(my.data = vpd.PISY[c(1:32891),],
@@ -173,7 +173,7 @@ PISY.vpd <- analyze.wavelet(my.data = vpd.PISY[c(1:32891),],
                           lowerPeriod = 2, upperPeriod = 24*365*2,
                           dj = 1/20,
                           loess.span = 0)
-saveRDS(PISY.vpd, "E:/MoonWood/Data/Zweifel/VPD_60min/waves/PISY_vpd.Rda")
+saveRDS(PISY.vpd, "PISY_vpd.Rda")
 
 vpd.PCAB <- ALL[ALL$Species == "PCAB",]
 PCAB.vpd <- analyze.wavelet(my.data = vpd.PCAB[c(1:38450),],
@@ -181,4 +181,4 @@ PCAB.vpd <- analyze.wavelet(my.data = vpd.PCAB[c(1:38450),],
                           lowerPeriod = 2, upperPeriod = 24*365*2,
                           dj = 1/20,
                           loess.span = 0)
-saveRDS(PCAB.vpd, "E:/MoonWood/Data/Zweifel/VPD_60min/waves/PCAB_vpd.Rda")
+saveRDS(PCAB.vpd, "PCAB_vpd.Rda")
